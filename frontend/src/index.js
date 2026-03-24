095eba56ab6036562eea88d3439584e25bd02c9c
saranshsahu123 <saranshsahu532@gmail.com>
2026-03-24 09:31:22 +0530

    updated file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import HomePage from './landingPage/Home/HomePage';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+<HomePage/>
   </React.StrictMode>
 );
 
